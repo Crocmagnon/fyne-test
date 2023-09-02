8d7d0387225c154aef9539b66fdd559c342e6513
Gabriel Augendre <gabriel@augendre.info>
2023-09-02 13:31:37 +0200

    use binding

--- main.go
+++ main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/data/validation"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello, world")
-	entry, label := makeUI()
-	w.SetContent(container.NewVBox(entry, label))
+	entry, label, errLabel := makeUI()
+	w.SetContent(container.NewVBox(entry, label, errLabel))
 	w.Resize(fyne.NewSize(200, 200))
 	w.ShowAndRun()
 }
 
-func makeUI() (*widget.Entry, *widget.Label) {
-	in := widget.NewEntry()
-	out := widget.NewLabel("Hello, world!")
+func makeUI() (*widget.Entry, *widget.Label, *widget.Label) {
+	str := binding.NewString()
+	in := widget.NewEntryWithData(str)
+	in.Validator = validation.NewRegexp(`^\w+$`, "invalid name")
 
-	in.OnChanged = func(name string) {
-		if name == "" {
-			name = "world"
+	str.Set("world")
+
+	formatted := binding.StringToStringWithFormat(str, "Hello, %v!")
+	out := widget.NewLabelWithData(formatted)
+
+	errMsg := binding.NewString()
+	in.SetOnValidationChanged(func(err error) {
+		if err == nil {
+			errMsg.Set("")
+			return
 		}
-		out.SetText(fmt.Sprintf("Hello, %v!", name))
-	}
-	return in, out
+
+		errMsg.Set(err.Error())
+	})
+	errLabel := widget.NewLabelWithData(errMsg)
+
+	return in, out, errLabel
 }
--- main_test.go
+++ main_test.go
@@ -3,20 +3,64 @@ package main
 import (
 	"fyne.io/fyne/v2/test"
 	"testing"
+	"time"
 )
 
 func TestUI(t *testing.T) {
-	in, out := makeUI()
+	in, out, err := makeUI()
 	got := out.Text
 	want := "Hello, world!"
 	if got != want {
 		t.Errorf("Incorrect initial greeting, got %q want %q", got, want)
 	}
 
+	in.SetText("")
 	test.Type(in, "Gabriel")
 	got = out.Text
 	want = "Hello, Gabriel!"
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+
+	if err.Text != "" {
+		t.Errorf("err label should be empty, found %q", err.Text)
+	}
+
+	test.Type(in, "hello world")
+	if err.Text == "" {
+		t.Error("err label should not be empty")
+	}
+}
+
+func TestSleep(t *testing.T) {
+	sleep := time.Millisecond
+	in, out, err := makeUI()
+
+	time.Sleep(sleep) // added sleep
+
+	got := out.Text
+	want := "Hello, world!"
+	if got != want {
+		t.Errorf("Incorrect initial greeting, got %q want %q", got, want)
+	}
+
+	in.SetText("")
+	test.Type(in, "Gabriel")
+
+	time.Sleep(sleep) // added sleep
+
+	got = out.Text
+	want = "Hello, Gabriel!"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	if err.Text != "" {
+		t.Errorf("err label should be empty, found %q", err.Text)
+	}
+
+	test.Type(in, "hello world")
+	if err.Text == "" {
+		t.Error("err label should not be empty")
 	}
 }
